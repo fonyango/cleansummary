25b99e028c15f45f9137953597c4b5be02ad15f7
fwonyango <francis@thehillcroftgroup.com>
2023-09-17 22:45:35 +0300

    Feature | Optimize code and add tests

---
+++ test_clean_summary.py
@@ -0,0 +1,65 @@
+import unittest
+import pandas as pd
+import numpy as np
+import logging
+from cleansummary import CleanSummary
+import matplotlib.pyplot as plt
+import warnings
+
+# Set the Matplotlib logging level to WARNING
+logging.getLogger('matplotlib').setLevel(logging.WARNING)
+
+class TestCleanSummary(unittest.TestCase):
+
+    def setUp(self):
+        # Sample DataFrame for testing
+        data = pd.DataFrame({
+            'A': [1, 2, 3, 4, 5],
+            'B': [None, 7, 8, None, 10],
+            'C': ['queen', 'king', 'duke', 'duchess', 'monarch'],
+            'D': [None, None, None, None, None]
+        })
+        self.clean_summary = CleanSummary(data)
+        logging.basicConfig(level=logging.DEBUG)
+
+    def test_percentage_missing(self):
+        # Test the result of percentage_missing method
+        result = self.clean_summary.percentage_missing()
+        self.assertIsInstance(result, pd.DataFrame)
+        self.assertTrue('variable' in result.columns)
+        self.assertTrue('missing' in result.columns)
+
+    def test_check_skewness(self):
+        variable = 'B'
+        self.clean_summary.check_skewness(variable)
+        # Check if a figure was created
+        self.assertIsNotNone(plt.gcf())
+
+    def test_get_statistical_summary(self):
+        # Test if the result is a dataframe
+        result = self.clean_summary.get_statistical_summary()
+        self.assertIsInstance(result, pd.DataFrame)
+
+    def test_get_statistical_summary_numerical(self):
+        # Test if the result is a dataframe
+        result = self.clean_summary.get_statistical_summary(variableType='numerical')
+        self.assertIsInstance(result, pd.DataFrame)
+
+    def test_get_statistical_summary_categorical(self):
+        # Test if the result is a dataframe
+        result = self.clean_summary.get_statistical_summary(variableType='categorical')
+        self.assertIsInstance(result, pd.DataFrame)
+        self.assertTrue('mean' not in result.columns)
+        self.assertTrue('std' not in result.columns)
+        self.assertTrue('min' not in result.columns)
+        self.assertTrue('25%' not in result.columns)
+        self.assertTrue('50%' not in result.columns)
+        self.assertTrue('75%' not in result.columns)
+        self.assertTrue('max' not in result.columns)
+        self.assertTrue('median' not in result.columns)
+        self.assertTrue('skewness' not in result.columns)
+        self.assertTrue('num_outliers' not in result.columns)
+        self.assertTrue('normality' not in result.columns)
+        
+if __name__ == '__main__':
+    unittest.main()
